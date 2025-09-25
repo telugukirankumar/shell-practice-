e480cc42269075310dc4503fde0887719efbfb5d
telugukirankumar <telugukirankumar9121@gmail.com>
2025-09-25 10:11:05 +0530

    conditions

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -3,8 +3,8 @@
 NUMBER1=$1
 if [ $NUMBER -lt 10 ] ;
    echo "given number $NUMBER1 is less than 10"
-elif
-    echo "given number $NUMBER1 is grater than 10"
+else
+   echo "given number $NUMBER1 is grater than 10"
 
 fi 
 
