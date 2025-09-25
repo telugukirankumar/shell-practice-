fa63598195ef9d2dbd0d2231bfaa6573130eb82b
telugukirankumar <telugukirankumar9121@gmail.com>
2025-09-25 10:15:46 +0530

    conditions

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # this is for conditions practice
 NUMBER1=$1
-if [ $NUMBER -lt 10 ] ; then
+if [ $NUMBER -lt 10 ]; then
    echo "given number $NUMBER1 is less than 10"
 else
    echo "given number $NUMBER1 is grater than 10"
