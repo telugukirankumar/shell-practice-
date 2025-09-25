7bae2a1fb80ff82580ad27cb5f9e4971fb248f9a
telugukirankumar <telugukirankumar9121@gmail.com>
2025-09-25 10:09:22 +0530

    conditions

---
+++ 05-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# this is for conditions practice
+NUMBER1=$1
+if [ $NUMBER -lt 10 ] ;
+   echo "given number $NUMBER1 is less than 10"
+elif
+    echo "given number $NUMBER1 is grater than 10"
+
+fi 
+
