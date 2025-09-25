7b755369deb04f9c59dbe01cb6f0380d88dcfb97
telugukirankumar <telugukirankumar9121@gmail.com>
2025-09-25 11:32:24 +0530

    conditions

---
+++ 06-condtitions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then 
+    echo "you don't have rights to run this script please switch to root previlges"
+    exit 1
+if   
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "mysql installation failed"
+    exit 1
+else 
+    echo "mysql installed successfully"
+fi
